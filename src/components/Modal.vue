458deef8d82b3a7e464de023441c1af7b62dec2e
“coreyalvarez” <coreyalvarez1989@gmail.com>
2022-04-20 19:56:06 -0500

    adds modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1 class="text-3xl text-green-500">{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'text']
+}
+</script>
+
+
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1 {
+        font-weight: bolder;
+    }
+</style>
