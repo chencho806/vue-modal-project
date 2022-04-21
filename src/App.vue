497eb3de6f465ce675bba2443d118be189980e9a
“coreyalvarez” <coreyalvarez1989@gmail.com>
2022-04-20 19:56:25 -0500

    imports modal component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
  <h1>{{ title }}</h1>
+ <ModalVue 
+    header="Sign Up For Mailing List" 
+    text="Be on the look out for our giveaways!"/>
 </template>
 
 <script>
-
+import ModalVue from "./components/Modal.vue"
 
 export default {
   name: 'App',
+  components: { ModalVue },
   data() {
     return {
       title: '¡Hola, Pendejo!'
     }
   }
 }
-</script>
+</script> 
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center; 
   color: #2c3e50;
   margin-top: 60px;
 }
+
 
 </style>
